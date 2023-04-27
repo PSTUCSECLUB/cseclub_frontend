fbfed671cf06f10324c6341d491d64e0e0cd7886
Jamatul Islam <newstartjam@gmail.com>
2023-04-27 14:26:35 +0600

    feat: pagination

--- src/components/admin/tabs/events.jsx
+++ src/components/admin/tabs/events.jsx
@@ -1,11 +1,26 @@
 import PanelCard from "@/components/cards/panelCard";
-import React from "react";
+import Paginate from "@/components/paginate/paginate";
+import React, { useState } from "react";
 
-export default function Events({ events }) {
+export default function Events({ events, itemsPerPage = 8 }) {
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentEvents = events.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(events.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % events.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
   return (
     <div className="admin__tab admin__events">
       <div className="admin__events__wrapper">
-        {events.map((e, i) => {
+        {currentEvents.map((e, i) => {
           return (
             <PanelCard
               key={i}
@@ -15,6 +30,9 @@ export default function Events({ events }) {
             />
           );
         })}
+      </div>
+      <div className="admin__events__paginate__wrapper">
+        <Paginate handlePageClick={handlePageClick} pageCount={pageCount} />
       </div>
     </div>
   );
